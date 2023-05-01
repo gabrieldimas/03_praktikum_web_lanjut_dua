78e52cd64f7365f8d4b747ab55016cd1b9d6b667
Gabriel Dimas <93063866+gabrieldimas@users.noreply.github.com>
2023-05-01 15:02:47 +0700

    jobsheet 3

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,137 @@
+@extends('template.main')
+@section('kongten')
+     <!--Main content-->
+     <section class="container margin-bottom-50">
+      <div class="row">
+        <div class="tm-gallery col-lg-12">
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">A Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">B Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">C Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">E Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">S Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">G Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>                    
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+        </div>        
+      </div>
+
+      <!--banner-->
+      <div class="tm-banner">
+        <h2 class="tm-banner-title">Maecenas</h2>
+        <p class="tm-banner-description">Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium</p>
+        <a href="#" class="tm-banner-link"><i class="fa fa-play"></i></a>
+      </div>
+    </section> <!-- Main content -->
+@endsection
